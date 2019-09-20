de3d838dbebf219131d0bf7aae35100e85fb9f19
Mars Le <hp.mars.le@gmail.com>
2019-09-20 05:05:43 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,11 @@
 class Meal
-
+  attr_accessor :waiter, :customer, :total, :tip 
+  def initialize(waiter,customer,total,tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip 
+    
+    
+  end
 end
