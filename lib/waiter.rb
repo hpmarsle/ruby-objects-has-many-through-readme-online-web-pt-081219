eb43c170dc6f5ff34de9fb7147b594532710c466
Mars Le <hp.mars.le@gmail.com>
2019-09-20 05:13:51 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -12,4 +12,17 @@ class Waiter
    def self.all 
     @@all 
   end 
+  def new_meal(customer,total,tip=0)
+    Meal.new(self,customer,total,tip)
+  end 
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self 
+    end 
+  end 
+  
+  def best_tipper 
+    # meals.map{|meal|meal.customer}
+  end 
 end
